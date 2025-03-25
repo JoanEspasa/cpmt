41156ec51980c1c853ad56e0c2c1c83eb1600792
Joan Espasa Arxer <cat.os.mandros@gmail.com>
2025-03-25 09:32:10 +0000

    fix

--- solver.py
+++ solver.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import z3
 
 # Add the build directory to Python's module search path
 build_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'build')
 sys.path.insert(0, build_dir)
+z3_dir = os.path.dirname(z3.__file__) # the directory where the Z3 Python module is located
+sys.path.insert(0, os.path.join(z3_dir, 'lib'))
 
 import z3solver
 
